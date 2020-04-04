318de1da0462174b0c1dfc8b2f120864cfed0d87
hansronald <robin.lindstrom@gmail.com>
2020-04-04 07:55:02 +0200

    Change to here instead of base_path

--- Scripts/common_settings.R
+++ Scripts/common_settings.R
@@ -1,7 +1,15 @@
 library(tidyverse)
 library(janitor)
+library(here)
 
 theme_set(theme_classic())
 
-base_path = "~/Google Drive/Skola/SRC/Thesis/Code/Scripts/output/"
-image_path = "~/Google Drive/Skola/SRC/Thesis/Code/Scripts/output/images/"
+#base_path = "~/Google Drive/Skola/SRC/Thesis/Code/Scripts/output/"
+#image_path = "~/Google Drive/Skola/SRC/Thesis/Code/Scripts/output/images/"
+
+# Functions
+scaleFUN <- function(tx) { 
+  div <- findInterval(as.numeric(gsub("\\,", "", tx)), 
+                      c(0, 1e3, 1e6, 1e9, 1e12) )
+  paste0(round( as.numeric(gsub("\\,","",tx))/10^(3*(div-1)), 2), 
+         c("","K","M","B","T")[div] )}

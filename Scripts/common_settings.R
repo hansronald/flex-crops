33349d822b21d11da6ca2cc9657219bb29361e7a
hansronald <robin.lindstrom@gmail.com>
2020-04-04 20:14:41 +0200

    Changed T (for Tera) to 1000B

--- Scripts/common_settings.R
+++ Scripts/common_settings.R
@@ -12,4 +12,4 @@ scaleFUN <- function(tx) {
   div <- findInterval(as.numeric(gsub("\\,", "", tx)), 
                       c(0, 1e3, 1e6, 1e9, 1e12) )
   paste0(round( as.numeric(gsub("\\,","",tx))/10^(3*(div-1)), 2), 
-         c("","K","M","B","T")[div] )}
+         c("","K","M","B","000B")[div] )}

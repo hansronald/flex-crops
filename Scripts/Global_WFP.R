9f3369dfd004f05a66a0721fb970d3a349b3e413
hansronald <robin.lindstrom@gmail.com>
2020-04-06 03:06:31 +0200

    Minor changes in color palette.

--- Scripts/Global_WFP.R
+++ Scripts/Global_WFP.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(janitor)
 library(here)
 library(ggforce) # for 'geom_arc_bar'
+library(ggpubr)
 source(here("Scripts", "common_settings.R"))
 
 crop_wfp = read_csv("/Users/robinlindstrom/Downloads/Global avg wfp.csv") %>% 
@@ -12,7 +13,7 @@ FAO_codes = read_csv("/Users/robinlindstrom/Downloads/FAOSTAT_data_4-4-2020.csv"
   clean_names() %>% 
   select(item_code, item)
 
-# crop_production_data = read_csv("/Users/robinlindstrom/Google Drive/Skola/SRC/Thesis/Code/Output processed data/crop_production_data_processed.csv")
+crop_production_data = read_csv("/Users/robinlindstrom/Google Drive/Skola/SRC/Thesis/Code/Output processed data/crop_production_data_processed.csv")
 
 production_2018 = crop_production_data %>% 
   filter(year == 2018,
@@ -49,30 +50,13 @@ global_wfp = crop_prod_wfp %>%
 
 write_csv(global_wfp, here("Output data", "global_wfp.csv"))  
 
-global_wfp %>%
 
+global_wfp %>%
   mutate(item = as.factor(item),
-         item = fct_rev((fct_reorder(item, rown, .desc = FALSE)))) %>% 
-  
-  # mutate(end = 2 * pi * cumsum(wfp_share)/sum(wfp_share),
-  #        start = lag(end, default = 0),
-  #        middle = 0.5 * (start + end),
-  #        hjust = ifelse(middle > pi, 1, 0),
-  #        vjust = ifelse(middle < pi/2 | middle > 3 * pi/2, 0, 1)) %>% 
-  # 
-  # geom_arc_bar(aes(x0 = 0, y0 = 0, r0 = 0, r = 1,
-  #                  start = start, end = end, fill = item)) +
-  # geom_text(aes(x = 1.05 * sin(middle), y = 1.05 * cos(middle), label = wfp_share,
-  #               hjust = hjust, vjust = vjust)) +
-  # coord_fixed() +
-  # scale_x_continuous(limits = c(-1.5, 1.4),  # Adjust so labels are not cut off
-  #         name = "", breaks = NULL, labels = NULL) +
-  # scale_y_continuous(limits = c(-1, 1),      # Adjust so labels are not cut off
-  #                   name = "", breaks = NULL, labels = NULL)
-  
+         item = ((fct_reorder(item, rown, .desc = TRUE)))) %>% 
   ggplot(aes(x = 1, y = total_wfp, fill = item)) +
   geom_bar(stat = "identity", color = "black") +
-  coord_polar("y", start=0) +
+  coord_polar("y", start=0, direction = -1) +
   theme(axis.ticks=element_blank(),
         axis.text.y=element_blank(),
         axis.text.x=element_text(colour='black'),
@@ -84,25 +68,49 @@ global_wfp %>%
   scale_y_continuous(breaks=cumsum(global_wfp$total_wfp) - global_wfp$total_wfp / 2,
                      labels= paste0(round(global_wfp$wfp_share, 2)*100, "%")) +
   scale_fill_brewer(palette="Paired") +
-  labs(title = "Share of total global water footprint (2018)")
+  labs(title = "Share of total global water footprint (2018)", tag = "(a)")
 
 ggsave(here("Output images", "global_wfp_share.png"), dpi = 500)
 
 scale_value = 1
 
+plot(brewer.pal(1, "Paired"))
+
+temp <- 1:length(crop_colors)
+barplot(temp, col = crop_colors)
+
+crop_colors = brewer.pal(9, "Paired")
+
+names(crop_colors) = levels(fct_relevel(as.factor(global_wfp$item), c("Other", "Barley", "Seed cotton", "Oil palm fruit", "Sugar cane",
+                                                                      "Soybeans", "Rice, paddy", "Wheat", "Maize")))
+  
+  levels(as.factor(global_wfp$item))
+
+
+  
+  levels(as.factor())
+
 global_wfp %>% 
   mutate(item = as.factor(item),
-         item = fct_rev(fct_reorder(item, rown))) %>% 
+         item = fct_reorder(item, rown, .desc = TRUE)) %>%
+  mutate(total_wfp = ifelse(item == "Other", 0, total_wfp)) %>% 
   ggplot(aes(x = item, y = total_wfp, fill = item)) +
   geom_bar(stat = "identity", width = 0.9*scale_value) +
+  geom_text(aes(label = round(total_wfp/1e9)), hjust = 1.1, size = 2.5) +
   coord_flip() +
-  labs(title = "Global water footprint (2018)", x = "", y = "water footprint"~(m^3)) +
-  scale_y_continuous(labels = scaleFUN) +
+  labs(title = "Global water footprint (2018)", x = "", y = "water footprint"~(m^3), tag = "(b)") +
+  scale_y_continuous(breaks = scales::extended_breaks(n = 6), labels = scaleFUN) +
   scale_fill_brewer(palette="Paired") +
   theme_classic(base_size = 8*scale_value) +
   theme(aspect.ratio = 1*scale_value,
         #legend.key.size = unit(0.5*scale_value,"line"),
         legend.position = "none")
-  
+
 ggsave(here("Output images", "global_wfp.png"), height = 2.5*scale_value, width = 4*scale_value, dpi = 500)
-  
+
+# Combine
+library(gridExtra)
+
+ggarrange(g1, g2, common.legend = TRUE)
+grid.arrange(g1, g2)
+

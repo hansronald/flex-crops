0827dc885842504242ae54c6e11474840d087c76
hansronald <robin.lindstrom@gmail.com>
2020-04-10 14:47:50 +0200

    Added the function to make map data here

--- Scripts/common_settings.R
+++ Scripts/common_settings.R
@@ -13,3 +13,21 @@ scaleFUN <- function(tx) {
                       c(0, 1e3, 1e6, 1e9, 1e12) )
   paste0(round( as.numeric(gsub("\\,","",tx))/10^(3*(div-1)), 2), 
          c("","K","M","B","000B")[div] )}
+
+make_map_data = function(map_input_data){
+  # Join the map data with the crop data
+  library(rnaturalearth)
+  library(sf)
+  library(rgeos)
+  library(dplyr)
+  world <- ne_countries(scale = "small", returnclass = "sf")
+  
+  world_filtered = world %>% 
+    select(country = name_long, iso3_code = iso_a3, geometry) %>%
+    filter(!is.na(iso3_code)) %>% 
+    filter(country != "Antarctica") 
+  
+  map = full_join(world_filtered, map_input_data, by = "iso3_code") 
+  
+  return(map)
+}
